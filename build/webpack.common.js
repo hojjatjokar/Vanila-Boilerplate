0d64b4697277b8f8d05054667c3696dc01e93443
Hojjat Jokar <7152729+hojjatjokar@users.noreply.github.com>
2022-02-15 16:48:30 +0330

    chore: add bable config

---
+++ build/webpack.common.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Production',
+    }),
+  ],
+  module: {     
+    rules: [
+      {         
+        test: /\.m?js$/,         
+        exclude: /node_modules/,         
+        use: {           
+          loader: "babel-loader",
+            options: {             
+            presets: ['@babel/preset-env']          
+          }
+        }       
+      }     
+    ]
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+};
